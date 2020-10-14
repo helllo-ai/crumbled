b7311447b75aaedd1ece132da6509162feda83b7
helllo-ai <71385601+helllo-ai@users.noreply.github.com>
2020-10-14 09:45:25 +0530

    Add files via upload

---
+++ dustbin.js
@@ -0,0 +1,7 @@
+class dustbin extends Base
+{
+	constructor(x,y){
+		super(x,y,50,50)
+		this.image=loadImage("trashcan.png")
+	}
+	}
---
+++ paper.js
@@ -0,0 +1,7 @@
+class paper extends Base
+{
+	constructor(x,y,r){
+		super(x,y,r)
+		this.image=loadImage("paper.png")
+	}	
+}
